be562d57c0e88ff8993cd7b701dbd148f9b04de8
HugoBoxFr <preahugo@gmail.com>
2022-02-21 15:15:10 +0100

    init authentication

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,27 +1,80 @@
-import { Arg, ID, Mutation, Query, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  ID,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  UseMiddleware,
+} from "type-graphql";
 import { getRepository } from "typeorm";
 import { User, UserInput } from "../models/User.model";
+import * as argon2 from "argon2";
+import { isAuth } from "../utils/isAuth";
+import { MyContext } from "../context/MyContext";
+import { sign } from "jsonwebtoken";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver(User)
 export class UserResolver {
   private userRepo = getRepository(User);
 
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  async Me(@Ctx() { payload }: MyContext) {
+    return `Your user id : ${payload!.userId}`;
+  }
+
   @Query(() => [User])
-  async getUser(): Promise <User[]> {
+  async getUser(): Promise<User[]> {
     return await this.userRepo.find();
   }
 
- // on peut copier coller directement dans addTaskToProject
   @Mutation(() => User)
-  async addUser(
-    @Arg("data", () => UserInput) user: User): Promise<User> {
-        const newUser = User.create(user);
-        await newUser.save();
-        return newUser;
-    }
+  async addUser(@Arg("data", () => UserInput) user: User): Promise<User> {
+    const newUser = user;
+    newUser.password = await argon2.hash(user.password);
+    const userToSave = await this.userRepo.create(newUser).save();
 
-      // Get TAsk By ID
-  @Query(() => User, {nullable: true})
+    return userToSave;
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<LoginResponse> {
+    try {
+      const user = await this.userRepo.findOne({ where: { email: email } });
+      if (!user) {
+        throw new Error("No user found!");
+      }
+
+      // ok okokok
+
+      if (await argon2.verify(user.password, password)) {
+        return {
+          accessToken: sign({ userId: user.id }, "MySecretKey", {
+            expiresIn: "15m",
+          }),
+        };
+      } else {
+        throw new Error("Email or password incorrect !");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // Get TAsk By ID
+  @Query(() => User, { nullable: true })
   async getUserById(@Arg("id", () => ID) id: number): Promise<User> {
     try {
       const user = await this.userRepo.findOne(id);
@@ -32,27 +85,28 @@ export class UserResolver {
     }
   }
 
-     // Update
+  // Update
   @Mutation(() => User)
-  async updateTask(@Arg("data",()=> UserInput) user: UserInput, 
-  @Arg("id", () => ID) id: number): Promise<User> {
-    let findUser= await this.getUserById(id);
-    if (findUser){
-        findUser.username = user.username
-        findUser.save()
-    } 
+  async updateTask(
+    @Arg("data", () => UserInput) user: UserInput,
+    @Arg("id", () => ID) id: number
+  ): Promise<User> {
+    let findUser = await this.getUserById(id);
+    if (findUser) {
+      findUser.username = user.username;
+      findUser.save();
+    }
     return findUser;
   }
 
   // Delete Task
   @Mutation(() => Boolean)
   async deleteTask(@Arg("id", () => ID) id: number): Promise<boolean> {
-    let findUser= await this.getUserById(id);
-    if (findUser){
-       await findUser.remove()
-      return true
-    } 
-    return false
+    let findUser = await this.getUserById(id);
+    if (findUser) {
+      await findUser.remove();
+      return true;
+    }
+    return false;
   }
-
 }
